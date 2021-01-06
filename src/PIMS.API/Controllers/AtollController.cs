f6282ee00991abcf06db8a8f6e96d4cb589d1836
Mubah <nevoscarlet@gmail.com>
2021-01-06 09:45:45 +0500

    added atoll controller

---
+++ src/PIMS.API/Controllers/AtollController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PIMS.API.Data;
+using PIMS.API.Domain.Entities;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PIMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/atolls")]
+    public class AtollController : ControllerBase
+    {
+        private readonly PIMSContext _context;
+        private readonly ILogger _logger;
+
+        public AtollController(PIMSContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var atolls = await _context.Atolls.ToListAsync();
+
+            return Ok(atolls);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetAtoll(int id)
+        {
+            try
+            {
+               var atoll = await _context.Atolls.Where(a => a.Id == id).FirstOrDefaultAsync();
+                
+                return Ok(atoll);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        } 
+    }
+}
--- src/PIMS.API/Domain/Entities/PatientHistory.cs
+++ src/PIMS.API/Domain/Entities/PatientHistory.cs
@@ -3,11 +3,11 @@ namespace PIMS.API.Domain.Entities
     public class PatientHistory : EntityBase
     {
 #nullable enable
-        public string CancerTreatment { get; set; }
-        public string Medical { get; set; }
-        public string Surgical { get; set; }
-        public string Familial { get; set; }
-        public string Other { get; set; }
+        public string? CancerTreatment { get; set; }
+        public string? Medical { get; set; }
+        public string? Surgical { get; set; }
+        public string? Familial { get; set; }
+        public string? Other { get; set; }
 # nullable disable
 
         public int PatientId { get; set; }
